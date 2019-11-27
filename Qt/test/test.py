77e17200fd50e3482a6530b782c4038ce5e9fdb3
Ваше Имя <raznomazov_vm@mail.ru>
2019-11-27 19:47:31 +0200

    The Qt wasb finnished

--- Qt/test/test.py
+++ Qt/test/test.py
@@ -3,13 +3,13 @@ import sys
 from PySide2.QtWidgets import QApplication
 
 
-class MainProject:
+class test:
     def __init__(self):
         pass  # call __init__(self) of the custom base class here
 
 
 if __name__ == "__main__":
     app = QApplication([])
-    window = MainProject()
+    window = test()
     # window.show()
     sys.exit(app.exec_())
